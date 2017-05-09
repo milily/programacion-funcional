90377cd99aee27df21393c7f6152a21fd395c299
mily rivas <mrise.translations@gmail.com>
2017-05-09 14:29:05 -0300

    ejercicio listo

---
+++ assets/main.js
@@ -0,0 +1,15 @@
+l1 = [1, 2, 3];
+l2 = [2, 3, 4];
+
+function intersection(l1, l2){
+	var l3 = [];
+	for(var i = 0; i < l1.length; i++){
+	 for(var j = 0; i < l2.length; j++){
+	 	if(l1[i] == l2[i]){
+	 		l3.push(li[i]);
+
+	 }
+   }
+}
+  return l3;
+}
